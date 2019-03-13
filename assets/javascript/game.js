df45250ff514d02e5d51f14c4a41b1ecc2cf3f97
Ashell Fernando <ashellz2010@gmail.com>
2019-03-13 06:27:49 -0400

    started on logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,106 @@
+const alpha = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+];
+const bravo = [
+    'z',
+    'y',
+    'x',
+    'w',
+    'v',
+    'u',
+    't',
+    's',
+    'r',
+    'q',
+    'p',
+    'o',
+    'n',
+    'm',
+    'l',
+    'k',
+    'j',
+    'i',
+    'h',
+    'g',
+    'f',
+    'e',
+    'd',
+    'c',
+    'b',
+    'a'
+];
+
+//object to keep score. access it by score.wins, score.losses, score.guesses_left
+var score = {
+    wins: 0,
+    losses: 0,
+    guesses_left: 10,
+    userGuesses: []
+};
+//setting element IDs
+var wins_ui = document.getElementById('wins-ui'),
+    losses_ui = document.getElementById('losses-ui'),
+    guesses_rem = document.getElementById('guess_rem'),
+    user_tried = document.getElementById('user-guesses'),
+    compguess = (snow_btn = document.getElementById('snow-btn'));
+
+//main function to do everything... or should I go funception on it..? lol
+const mainFunc = input => {
+    var compGuess = alpha[
+        Math.floor(Math.random() * alpha.length)
+    ].toLowerCase();
+
+    score.guesses_left - userGuesses.length;
+
+    if (
+        score.userGuesses === '' ||
+        score.userGuesses[score.userGuesses.length - 1] != compGuess
+    ) {
+        score.losses++;
+        losses_ui.innerText = score.losses;
+    }
+
+    console.log(
+        `Computer Generated: ${compGuess} ${
+            score.userGuesses[score.userGuesses.length - 1]
+        } `
+    );
+};
+
+//get's the keyboard event
+document.onkeyup = event => {
+    mainFunc();
+    score.userGuesses.push(event.key);
+    console.log(`User Guessed: ${score.userGuesses}`);
+};
+
+console.log(`wins: ${score.wins}
+losses: ${score.losses}
+guesses left: ${score.guesses_left}
+User Guessed: ${score.userGuesses}
+`);
