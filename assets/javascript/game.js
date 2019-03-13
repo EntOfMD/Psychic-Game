5fb590d064aaab088314e6c645ec9cc7a6632345
Ashell Fernando <ashellz2010@gmail.com>
2019-03-13 18:04:49 -0400

    lowercased alphabet, structure change, new methods

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,107 @@
-//hardcore
-"use strict";
+'use strict';
 
 //the main object holding everything
 var block = {
-  abc: [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z"
-  ],
+    abc: [
+        'z',
+        'y',
+        'x',
+        'w',
+        'v',
+        'u',
+        't',
+        's',
+        'r',
+        'q',
+        'p',
+        'o',
+        'n',
+        'm',
+        'l',
+        'k',
+        'j',
+        'i',
+        'h',
+        'g',
+        'f',
+        'e',
+        'd',
+        'c',
+        'b',
+        'a'
+    ],
 
-  score: {
-    wins: 0,
-    losses: 0,
-    guesses_left: 10,
-    userGuesses: []
-  },
-  DOMs: {
-    wins_ui: document.getElementById("wins-ui"),
-    losses_ui: document.getElementById("losses-ui"),
-    guesses_rem: document.getElementById("guess_rem"),
-    user_tried: document.getElementById("user-guesses"),
-    snow_btn: document.getElementById("snow-btn")
-  },
+    score: {
+        wins: 0,
+        losses: 0,
+        guesses_left: 10,
+        userGuesses: [],
+        compGens: []
+    },
+    DOMs: {
+        wins_ui: document.getElementById('wins-ui'),
+        losses_ui: document.getElementById('losses-ui'),
+        guesses_rem: document.getElementById('guess_rem'),
+        user_tried: document.getElementById('user-guesses'),
+        snow_btn: document.getElementById('snow-btn')
+    },
 
-  reWriteStats: () => {
-    wins_ui.innerText = score.wins;
-    losses_ui.innerText = score.losses;
-    guesses_rem.innerText = score.guesses_left;
-    user_tried.innerText = score.userGuesses;
-  }
+    compGenGuess: () => {
+        var randomGen = block.abc[
+            Math.floor(Math.random() * block.abc.length)
+        ].toLowerCase();
+        block.score.compGens.push(randomGen);
+        console.log(
+            `time: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} Computer Generated: ${randomGen}`
+        );
+    },
+
+    reWriteStats: () => {
+        block.DOMs.wins_ui.innerText = block.score.wins;
+        block.DOMs.losses_ui.innerText = block.score.losses;
+        block.DOMs.guesses_rem.innerText = block.score.guesses_left;
+        block.DOMs.user_tried.innerText = `${block.score.userGuesses}`;
+    },
+
+    resetScore: () => {
+        block.score.wins = 0;
+        block.score.losses = 0;
+        block.score.guesses_left = 0;
+        block.score.userGuesses = [];
+    },
+
+    userGuessToDOM: () => {
+        block.score.userGuesses.map(guesses => {
+            block.DOMs.user_tried.innerText = block.score.userGuesses;
+        });
+    }
+};
+
+block.compGenGuess();
+block.reWriteStats();
+
+document.onkeyup = event => {
+    var userInput = event.key.toLowerCase();
+    block.userGuessToDOM();
+    var specialChar = block.abc.includes(userInput); //this returns true or false.
+    //this checks whether if the userInput is a special character or not
+    if (!specialChar) {
+        console.log(
+            `you pressed ${userInput}, please type only alphabet letters`
+        );
+    } else {
+        block.score.userGuesses.push(userInput);
+    }
+    console.log(block.score.userGuesses);
+
+    //do this if condition is true
+    while (block.score.userGuesses <= block.score.guesses_left) {
+        if (userInput == block.score.compGens) {
+            block.compGenGuess();
+            console.log('true');
+        }
+    }
+    //do this if the condition is false
+
+    // return userInput;
 };
